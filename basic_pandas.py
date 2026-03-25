4152e3e447ff503f853d7de5792b343761945915
Swapndeep Tomar <tomarsd50@gmail.com>
2026-03-25 19:10:22 +0530

    Series Slicing

--- basic_pandas.py
+++ basic_pandas.py
@@ -185,3 +185,24 @@ data["Salary (int)"]= data["Salary"].str.slice(start, stop, step)
 
 # display
 print(data.head(10))
+
+# importing pandas module 
+import pandas as pd 
+  
+# making data frame 
+data = pd.read_csv("https://media.geeksforgeeks.org/wp-content/uploads/nba.csv") 
+  
+# removing null values to avoid errors 
+data.dropna(inplace = True) 
+
+# start stop and step variables
+start, stop, step = 0, -2, 1
+
+# converting to string data type
+data["Salary"]= data["Salary"].astype(str)
+
+# slicing till 2nd last element
+data["Salary (int)"]= data["Salary"].str.slice(start, stop, step)
+
+# display
+data.head(10)
